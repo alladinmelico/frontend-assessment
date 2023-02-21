be2e4cbefb8727015c9d7dade224d69d8f7577bd
Alladin <melico.alladin@gmail.com>
2023-02-21 19:29:22 +0800

    assessment completed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,118 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import AccordionCard from './components/AccordionCard.vue'
+import cards from './assets/data.json'
+
+const activeCardIndex = ref(0)
+
+function handleActiveChange(index: number): void {
+  activeCardIndex.value = index === activeCardIndex.value ? -1 : index
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section class="container-fluid header">
+    <picture class="header__banner">
+      <source media="(max-width: 420px)" srcset="https://via.placeholder.com/600x600" />
+      <source media="(max-width: 720px)" srcset="https://via.placeholder.com/1920x650" />
+      <img src="https://via.placeholder.com/1920x650" alt="Placeholder image" />
+    </picture>
+    <Transition appear appear-active-class="fade-in">
+      <div class="header__text">
+        <h1>Hello Developer!</h1>
+        <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
+      </div>
+    </Transition>
+  </section>
+
+  <Transition appear appear-active-class="fade-enter-up">
+    <section class="container-lg accordion">
+      <div class="row row-cols-lg-3 gy-3 g-lg-5 accordion__container">
+        <AccordionCard v-for="(card, index) in cards" 
+          :title="card.title" 
+          :content="card.content" 
+          :index="index"
+          :is-active="activeCardIndex === index" 
+          @button-clicked="handleActiveChange"
+        />
+      </div>
+    </section>
+  </Transition>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style scoped lang="scss">
+#app {
+  margin: 0 auto;
+  text-align: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.header {
+  padding: 0;
+  position: relative;
+
+  &__banner {
+    width: 100%;
+    height: 100%;
+    display: flex;
+
+    img {
+      object-fit: cover;
+      height: auto;
+      width: 100%;
+    }
+  }
+
+  &__text {
+    text-align: center;
+    color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+
+    h1 {
+      font-size: 4rem;
+      font-weight: 700;
+    }
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.accordion {
+  padding: 2rem 1.5rem 2rem 1.5rem;
+
+  &__container {
+    justify-content: center;
+  }
+}
+
+.fade-enter-up {
+  animation: fade-enter-up 2s ease-in-out;
+}
+
+.fade-in {
+  animation: fade-in 1s ease-in-out;
+}
+
+@keyframes fade-enter-up {
+  from {
+    opacity: 50;
+    transform: translateY(50%)
+  }
+
+  to {
+    opacity: 100;
+    transform: translateY(0);
+  }
+}
+
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
 }
 </style>
---
+++ src/components/AccordionCard.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+interface Card {
+  title: string
+  content: string
+  image?: string
+  isActive: boolean
+  index: number
+}
+
+defineProps<Card>()
+
+defineEmits<{ (e: "button-clicked", id: number): void }>()
+</script>
+
+<template>
+  <div class="gy-5">
+    <div class="accordion__card">
+      <img :src="image || 'https://via.placeholder.com/400x300'" :alt="title" />
+      <div class="accordion__card__content">
+        <div v-html="content" :class="isActive || 'accordion__card__content--inactive'"></div>
+        <div v-if="!isActive" class="accordion__card__content--fade"></div>
+      </div>
+      <button type="button" @click="$emit('button-clicked', index)">
+        {{ isActive ? "Close" : "Read More" }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.accordion__card {
+  height: 100%;
+  min-width: 250px;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  text-align: center;
+  box-shadow: 0px 0px 10px 1px rgba(153, 153, 153, 0.19);
+  -webkit-box-shadow: 0px 0px 10px 1px rgba(153, 153, 153, 0.19);
+  -moz-box-shadow: 0px 0px 10px 1px rgba(153, 153, 153, 0.19);
+
+  &:hover {
+    box-shadow: 0px 0px 5px 0.5px rgba(153, 153, 153, 0.4);
+    -webkit-box-shadow: 0px 0px 5px 0.5px rgba(153, 153, 153, 0.4);
+    -moz-box-shadow: 0px 0px 5px 0.5px rgba(153, 153, 153, 0.4);
+  }
+
+  &__content {
+    margin: 1rem;
+    position: relative;
+
+    &--inactive {
+      max-height: 80px;
+      display: block;
+      overflow: hidden;
+    }
+
+    &--fade {
+      height: 80px;
+      width: 100%;
+      background-image: linear-gradient(to bottom, transparent, white);
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+
+  button {
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    border-radius: 2rem;
+    background-color: white;
+    border: solid 3px black;
+    text-transform: uppercase;
+    color: black;
+    font-weight: 800;
+    margin: 1rem;
+
+    &:hover {
+      color: white;
+      background-color: black;
+    }
+  }
+
+  img {
+    width: 100%;
+    border-radius: 0.25rem;
+  }
+
+  p {
+    margin-top: 1rem;
+  }
+}
+</style>
